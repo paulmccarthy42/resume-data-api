53827ebe13a5ac0d6f9ea219cf9a785e886c32fc
Paul McCarthy <paul.mccarthy@avant.com>
2018-01-09 20:24:00 -0600

    student.as_json baseline

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,25 @@ class Student < ApplicationRecord
   has_many :educations
   has_many :skills
   has_many :capstones
+
+  def as_json
+    {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      phone_number: phone_number,
+      short_bio: short_bio,
+      linkedin_url: linkedin_url,
+      twitter_handle: twitter_handle,
+      personal_blog_url: personal_blog_url,
+      online_resume_url: online_resume_url,
+      github_url: github_url,
+      photo: photo,
+      experiences: experiences,
+      educations: educations,
+      skills: skills,
+      capstones: capstones
+    }
+
+  end
 end
