063dc26820fb7851b828edb6dc54abc631668626
Paul McCarthy <paul.mccarthy@avant.com>
2018-01-09 21:03:40 -0600

    remove validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
   has_secure_password 
-  validates :name, presence: true 
-  validates :email, presence: true, uniqueness: true 
+  # validates :name, presence: true 
+  # validates :email, presence: true, uniqueness: true 
   
   has_many :experiences
   has_many :educations
