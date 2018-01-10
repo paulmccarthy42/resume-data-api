e4378dd8fd86f2faf2a7af21733d01519d3ced6b
Jaewon Park <eliana.jaewon@gmail.com>
2018-01-09 18:30:56 -0600

    create models

---
+++ db/migrate/20180110002332_create_experiences.rb
@@ -0,0 +1,14 @@
+class CreateExperiences < ActiveRecord::Migration[5.1]
+  def change
+    create_table :experiences do |t|
+
+      t.date :start_date
+      t.date :end_date
+      t.string :job_title
+      t.string :company_name
+      t.text :details
+
+      t.timestamps
+    end
+  end
+end
