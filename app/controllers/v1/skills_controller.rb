8f4a22217d3a1b105c55bfbd29c1322b3dd55b42
benjaminsinaiko <ben.jamin@outlook.com>
2018-01-09 19:34:06 -0600

    Add Skills controller

---
+++ app/controllers/v1/skills_controller.rb
@@ -0,0 +1,41 @@
+class V1::SkillsController < ApplicationController
+  def index
+    skills = Skill.all
+    render json: skills.as_json
+  end
+
+  def create
+    skill = Skill.new(
+      skill_name: params["skill_name"]
+      )
+    if skill.save
+      render json: skill.as_json
+    else
+      render json: {error: skill.errors.full_messages}, status: :bad_request
+    end
+  end
+
+  def show
+    skill_id = params["id"]
+    skill = Skill.find_by(id: skill_id)
+    render json: skill.as_json
+  end
+
+  def update
+    skill_id = params["id"]
+    skill = Skill.find_by(id: skill_id)
+    skill.skill_name = params["skill_name"] || skill.skill_name
+    if student.save
+      render json: student.as_json
+    else
+      render json: {error: skill.errors.full_messages}, status: :bad_request
+    end
+  end
+
+  def destroy
+    skill_id = params["id"]
+    skill = Skill.find_by(id: skill_id)
+    skill.destroy
+    render json: {message: "Later skills..."}
+  end
+end
